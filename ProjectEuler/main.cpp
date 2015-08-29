93ecd8a2b5a2202da58e44732fdfd5528c098228
Lokesh Kumar <lokeshk@gmail.com>
2015-08-29 12:04:39 -0700

    Initialized git repository with project files

---
+++ ProjectEuler/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ProjectEuler
+//
+//  Created by Lokesh Kumar on 8/29/15.
+//  Copyright (c) 2015 LK. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
